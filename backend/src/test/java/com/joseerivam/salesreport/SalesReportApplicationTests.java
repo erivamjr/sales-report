87861445228a43d8e6536bf75695fa76bc0cd6bc
José Erivam <erivamdev@gmail.com>
2022-09-05 19:15:51 -0300

    Project created

---
+++ backend/src/test/java/com/joseerivam/salesreport/SalesReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseerivam.salesreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
