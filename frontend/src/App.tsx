222c412ff11015b2e06984555081523b21a96c36
José Erivam <erivamdev@gmail.com>
2022-09-05 19:52:29 -0300

    first component button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./components/notificationButton"
 
 function App() {
   return (
-    <h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
